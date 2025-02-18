c65d29d0c2ef2c6b5744a8b2613edb4e0421d425
Savvas <Savvas_pantelis@student.uml.edu>
2025-02-17 21:32:38 -0500

    Bug fixing

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -5,8 +5,16 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class Calculator {
+	    
 	    final static int umlNUMBER1 = 3;
 	        
+	        /**
+		 *      * Returns the sum of two integers.
+		 *           *
+		 *                * @param a the first number
+		 *                     * @param b the second number
+		 *                          * @return the sum of a and b
+		 *                               */
 	        @Cacheable("sum")
 		    public int sum(int a, int b) {
 			            return a + b;
