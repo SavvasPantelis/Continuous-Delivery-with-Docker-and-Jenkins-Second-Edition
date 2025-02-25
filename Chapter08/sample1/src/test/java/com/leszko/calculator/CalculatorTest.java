c026d04904a2debe952ad55b93192b875e20a5f9
Savvas <Savvas_pantelis@student.uml.edu>
2025-02-24 20:57:59 -0500

    fixed checkstyle issue

--- Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
+++ Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
@@ -1,12 +1,20 @@
 package com.leszko.calculator;
+
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
+/**
+ *  * Unit tests for the Calculator class.
+ *   */
 public class CalculatorTest {
-     private Calculator calculator = new Calculator();
+	    private Calculator calculator = new Calculator();
 
-     @Test
-     public void testSum() {
-          assertEquals(5, calculator.sum(2, 3));
-     }
+	        /**
+		 *      * Test to verify the sum method of Calculator.
+		 *           */
+	        @Test
+		    public void testSum() {
+			            assertEquals(5, calculator.sum(2, 3));
+				        }
 }
+
