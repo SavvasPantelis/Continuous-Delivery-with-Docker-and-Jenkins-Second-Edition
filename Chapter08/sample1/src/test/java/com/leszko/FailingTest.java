627e7144d3560f29fd77fefeece16df8c1c0debb
Savvas <Savvas_pantelis@student.uml.edu>
2025-03-17 22:47:06 -0400

    Added failing test

---
+++ Chapter08/sample1/src/test/java/com/leszko/FailingTest.java
@@ -0,0 +1,13 @@
+package calculator;
+
+import org.junit.Test;
+import static org.junit.Assert.fail;
+
+public class FailingTest {
+
+	            @Test
+		                    public void shouldAlwaysFail() {
+					                                    fail("Intentionally failing to demonstrate pipeline failure.");
+									                                        }
+}
+
