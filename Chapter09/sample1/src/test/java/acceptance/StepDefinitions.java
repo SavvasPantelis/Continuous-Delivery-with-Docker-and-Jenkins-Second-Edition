771e2d56b7bac93678536b7aed409b790fd478eb
Savvas <Savvas_pantelis@student.uml.edu>
2025-04-01 00:39:10 -0400

    fixes

--- Chapter09/sample1/src/test/java/acceptance/StepDefinitions.java
+++ Chapter09/sample1/src/test/java/acceptance/StepDefinitions.java
@@ -4,7 +4,6 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.springframework.web.client.RestTemplate;
-
 import static org.junit.Assert.assertEquals;
 
 /** Steps definitions for calculator.feature */
@@ -16,35 +15,22 @@ public class StepDefinitions {
     private String a;
     private String b;
     private String result;
-
-    @Given("^I have two numbers: (.*) and (.*)$")
+			  
+    @Given("^I have dividend and divisor: (.*) and (.*)$")
     public void i_have_two_numbers(String a, String b) throws Throwable {
         this.a = a;
-        this.b = b;
+	this.b = b;
     }
 
-    @When("^the calculator sums them$")
-    public void the_calculator_sums_them() throws Throwable {
-        String url = String.format("%s/sum?a=%s&b=%s", server, a, b);
-        result = restTemplate.getForObject(url, String.class);
-    }
+    @When("^the calculator divides them$")
+    public void the_calculator_divides_them() throws Throwable {
+        String url = String.format("%s/div?a=%s&b=%s", server, a, b);
+	result = restTemplate.getForObject(url, String.class);
+								    }
 
-    @Then("^I receive (.*) as a result$")
-    public void i_receive_as_a_result(String expectedResult) throws Throwable {
+    @Then("^I receive (.*) as the quotient$")
+    public void i_receive_as_the_quotient(String expectedResult) throws Throwable {
         assertEquals(expectedResult, result);
     }
-    @Given("^I have dividend and divisor: (.*) and (.*)$")
-    public void i_have_dividend_and_divisor(String a, String b) throws Throwable {
-	this.a = a;
-        this.b = b;
-    }
-   @When("^the calculator divides them$")
-   public void the_calculator_divides_them() throws Throwable {
-       String url = String.format("%s/div?a=%s&b=%s", server, a, b);
-       result = restTemplate.getForObject(url, String.class);
-    }
-   @Then("^I receive (.*) as the quotient$")
-   public void i_receive_as_the_quotient(String expectedResult) throws Throwable {
-       assertEquals(expectedResult, result);
-    } 
 }
+
